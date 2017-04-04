29f97450840360d305db2864f55832298770c4dd
abinashnarendiran <abinash.narendiran@uoit.net>
2017-04-03 23:46:57 -0400

    Commit part of the sub menu

---
+++ Controller.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.fxml.*;
+
+import java.io.*;
+
+
+public class Controller {
+
+
+
+    @FXML
+    public void Logout(ActionEvent e) throws IOException {
+
+    }
+
+
+    public void Parks(ActionEvent e)throws IOException{
+
+    }
+
+    public void Gas(ActionEvent e)throws IOException{
+
+    }
+
+    public void Restaurants(ActionEvent e)throws IOException{
+
+    }
+
+    public void Hotels(ActionEvent e)throws IOException{
+
+    }
+
+    public void Bank(ActionEvent e)throws IOException{
+
+    }
+
+    public void Shopping(ActionEvent e)throws IOException{
+
+    }
+}
+
+
+
