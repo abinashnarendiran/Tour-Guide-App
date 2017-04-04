f8688aede807960b7cd7c322e661f49d750a4816
Saijeeshan Ketheeswaran <saijeeshan.ketheeswaran@uoit.net>
2017-04-04 15:53:14 -0400

    Updated Home Menu and Added Video Tab

--- Controller.java
+++ Controller.java
@@ -1,43 +1,74 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.fxml.*;
 
 import java.io.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    Stage stage;
+
+    @FXML
+    private MediaView mediaView;
+    private MediaPlayer mediaPlayer;
+    private Media media;
+
+    @FXML
+    private MediaView mediaView2;
+    private MediaPlayer mediaPlayer2;
+    private Media media2;
 
 
-public class Controller {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String path = new File("src/sample/videos/beijing.mp4").getAbsolutePath();
+        media = new Media(new File(path).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
 
+        String path2 = new File("src/sample/videos/rome.mp4").getAbsolutePath();
+        media2 = new Media(new File(path2).toURI().toString());
+        mediaPlayer2 = new MediaPlayer(media2);
+        mediaView2.setMediaPlayer(mediaPlayer2);
+    }
 
+    public void play(ActionEvent e) throws IOException {
+        mediaPlayer.play();
+        mediaPlayer2.pause();
+    }
+
+    public void pause(ActionEvent e) throws IOException {
+        mediaPlayer.pause();
+    }
+
+    public void play2(ActionEvent e) throws IOException {
+        mediaPlayer2.play();
+        mediaPlayer.pause();
+    }
+
+    public void pause2(ActionEvent e) throws IOException {
+        mediaPlayer2.pause();
+    }
+
+    public void setStage(Stage stage){
+
+        this.stage = stage;
+    }
+
+    @FXML
+    public void Search(ActionEvent e) throws IOException {
+
+    }
 
     @FXML
     public void Logout(ActionEvent e) throws IOException {
-
-    }
-
-
-    public void Parks(ActionEvent e)throws IOException{
-
-    }
-
-    public void Gas(ActionEvent e)throws IOException{
-
-    }
-
-    public void Restaurants(ActionEvent e)throws IOException{
-
-    }
-
-    public void Hotels(ActionEvent e)throws IOException{
-
-    }
-
-    public void Bank(ActionEvent e)throws IOException{
-
-    }
-
-    public void Shopping(ActionEvent e)throws IOException{
 
     }
 }
--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
 package sample;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        this.stage = primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = fxmlLoader.load();
+        ((Controller) fxmlLoader.getController()).setStage(primaryStage);
+
         primaryStage.setTitle("Tour Guide App");
         primaryStage.setScene(new Scene(root, 690, 490));
         primaryStage.setResizable(false);
